1cd5506e893ef6dd44a7ea3512b1988ae2f54d83
mooremj558 <106920796+mooremj558@users.noreply.github.com>
2026-07-02 15:13:40 -0500

    Delete Analysis/cLoops2 directory

--- Analysis/cLoops2/cLoops2_callLoops_array_v3.sh
+++
@@ -1,3 +0,0 @@
-file=$(awk -v  awkvar="${SLURM_ARRAY_TASK_ID}" 'NR==awkvar' samples2.txt)
-
-cLoops2 callLoops -d ${file} -o ${file} -eps 5000,7500,10000 -minPts 5,20,50 -p 15 -w -j -hic -mcut 10000000 -max_cut
--- Analysis/cLoops2/cLoops2_diffLoops_array.sh
+++
@@ -1,3 +0,0 @@
-file=$(awk -v  awkvar="${SLURM_ARRAY_TASK_ID}" 'NR==awkvar' celllines.txt)
-
-cLoops2 callDiffLoops -tloop /scratch/mmoore/Epitherapy3D/analysis/HiC/4_loop/cloops_chr/merged_loops/${file}_DMSO_loops_CTCF.txt -cloop /scratch/mmoore/Epitherapy3D/analysis/HiC/4_loop/cloops_chr/merged_loops/${file}_DP_loops_CTCF.txt -td /scratch/mmoore/Epitherapy3D/analysis/HiC/4_loop/cloops/${file}_DMSO/ -cd /scratch/mmoore/Epitherapy3D/analysis/HiC/4_loop/cloops/${file}_DP/ -o diffloops/new_unsampled/${file}_DMSOvsDP -p 10 -mcut 10000000 -vmin="-1" -vmax=3
